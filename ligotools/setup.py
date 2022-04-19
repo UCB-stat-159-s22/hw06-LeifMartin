7414fb99fc53e023c822ed52eaff77a996fe401e
LeifMartin <leifsunde@rocketmail.com>
2022-04-19 02:54:05 +0000

    Attempt at making an adequate setup.py file for converting the module to a package.

---
+++ ligotools/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+VERSION='0.0.1'
+setup(
+    name="ligotools",
+	version=VERSION
+    author="Ligo Scientific Collaboration (LSC) and Leif-Martin Sæther Sunde",
+    author_email="lmsunde@berkeley.edu",
+	packages=find_packages()
+)
